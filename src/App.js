caec856d670e78a90b9409c65d43029cf4209e8c
ihor.osypov <ihor.osypov@geeksforless.com>
2021-03-17 18:36:14 +0200

    Added layout, Added components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        <h1>Hello</h1>
+      <Container className="pt-3">
+        <Row>
+          <Col>
+            Form
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            Posts
+            </Col>
+          <Col>
+            Fetched posts
+            </Col>
+        </Row>
+
       </Container>
     </div>
   );
